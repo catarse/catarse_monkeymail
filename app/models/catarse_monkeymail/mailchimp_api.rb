0d081c601922a35f8f3f6ab2c5adb21a9ad888d2
Antônio Roberto Silva <forevertonny@gmail.com>
2014-05-29 17:12:19 -0300

    we need to require mailchimp

--- app/models/catarse_monkeymail/mailchimp_api.rb
+++ app/models/catarse_monkeymail/mailchimp_api.rb
@@ -1,7 +1,9 @@
+require 'mailchimp'
+
 module CatarseMonkeymail
   class MailchimpApi
     def self.start
-      @mailchimp ||= Mailchimp::API.new(::CatarseSettings[:mailchimp_api_key])
+      @mailchimp ||= ::Mailchimp::API.new(::CatarseSettings[:mailchimp_api_key])
     end
   end
 end
